9797551133e78f3870d6cbc8f4bddbc04e5187b6
dap-7 <dawpod7@gmail.com>
2025-05-26 15:02:27 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <vector>
+#include <utility>
+
+std::pair<int, int> mucha, pajak;
+int wielkosc_planszy;
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
