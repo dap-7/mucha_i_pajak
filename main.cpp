3662811a40e193065f131e602ebc163419341e53
dap-7 <dawpod7@gmail.com>
2025-05-26 15:27:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <utility>
 #include <iostream>
+
 std::pair<int, int> mucha, pajak;
 int wielkosc_planszy;
 
@@ -9,7 +10,19 @@ int main()
 {
     srand(time(0)); // Inicjalizacja generatora liczb losowych
     std::cout << "Jaka wielkość planszy (3,5,7): ";
-    std::cin >> wielkosc_siatki;
+    std::cin >> wielkosc_planszy;
 
     return 0;
+}
+
+int losuj_poczatek_pajaka()
+{
+    int i;
+    //float xsr=static_cast<float>(5)/2;
+    std::vector<std::pair<int, int>> vec; // Deklaracja pustego wektora par liczb całkowitych
+    std::pair<int, int> nthPair;
+    for (i=0; i<wielkosc_planszy; i++){
+        vec.push_back({i, 1});
+        vec.push_back({i, wielkosc_planszy});
+    }
 }
