528f8ec2f1969630f10449ec770d8c1fee6b04af
polpotr <kacperjagiello2011@gmail.com>
2025-06-03 08:34:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -57,9 +57,19 @@ void pokazPlansze(){
 }
 
 void przesun(std::pair<int, int> kogo, int kierunek){
-    
+if(kierunek==0 && kogo.second < wielkosc_planszy){
+kogo.second++;
 }
-
+if(kierunek==1 && kogo.second > 1){
+kogo.second--;
+}
+if(kierunek==2 && kogo.first > 1){
+kogo.first--;
+}
+if(kierunek==3 && kogo.first > wielkosc_planszy){
+kogo.first--;
+}
+}
 int ruch_pajaka(){
     //losuj ruch pajaka
     int k = rand() % 4;
